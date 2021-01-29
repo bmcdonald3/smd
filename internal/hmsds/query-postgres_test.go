51a88df2c88f4da672d0b3499912a4d1df92a5bc
Mike Buchmann <mbuchmann@cray.com>
2021-01-29 14:27:24 -0600

    MIT License Update

--- internal/hmsds/query-postgres_test.go
+++ internal/hmsds/query-postgres_test.go
@@ -1,4 +1,24 @@
-// Copyright 2019-2020 Hewlett Packard Enterprise Development LP
+// MIT License
+//
+// (C) Copyright [2019-2021] Hewlett Packard Enterprise Development LP
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 
 package hmsds
 
@@ -37,7 +57,7 @@ func TestToPGQueryArgs(t *testing.T) {
     INSERT INTO components (
         id, type, state, flag, enabled, admin, role, subrole, nid, subtype, nettype, arch, class, reservation_disabled, locked)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ON DUPLICATE KEY UPDATE 
+    ON DUPLICATE KEY UPDATE
         state=VALUES(state),
         flag=VALUES(flag),
         subtype=VALUES(subtype),
@@ -49,7 +69,7 @@ func TestToPGQueryArgs(t *testing.T) {
     INSERT INTO components (
         id, type, state, flag, enabled, admin, role, subrole, nid, subtype, nettype, arch, class, reservation_disabled, locked)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-    ON DUPLICATE KEY UPDATE 
+    ON DUPLICATE KEY UPDATE
         state=VALUES(state),
         flag=VALUES(flag),
         subtype=VALUES(subtype),
