80cf75351b276056baed57d82e2dbfebb0a7a329
Manny Alvarez III <manuel.alvarez@hpe.com>
2021-06-01 17:00:49 -0500

    Fix component filtering for locking components
    CASMHMS-4865
    
    Description:
    The /locks/(un)lock API was not properly filtering components which was causing everything to get locked. This mod fixes the component filtering.

--- internal/hmsds/hmsds-postgres.go
+++ internal/hmsds/hmsds-postgres.go
@@ -4977,10 +4977,14 @@ func (d *hmsdbPg) UpdateCompLocksV2(f sm.CompLockV2Filter, action string) (sm.Co
 				result.Failure = append(result.Failure, fail)
 				delete(affectedMap, reservation.ID)
 			}
-			// No reservation found for this lock. Time to (un)lock the component.
-			for id, _ := range affectedMap {
-				affectedIds = append(affectedIds, id)
-			}
+		}
+		// No reservation found for thess locks. Time to (un)lock the component.
+		for id, _ := range affectedMap {
+			affectedIds = append(affectedIds, id)
+		}
+		if len(affectedIds) == 0 {
+			t.Rollback()
+			return result, sm.ErrCompLockV2CompReserved
 		}
 		// No reservation found for this lock. Time to (un)lock the component.
 		updatedIds, err := t.BulkUpdateCompResLockedTx(affectedIds, newVal)
